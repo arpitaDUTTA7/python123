cb11a26b9f541b73f151bbcf05f5155108535743
ArpitaD <arpitadutta670@gmail.com>
2022-10-30 00:40:13 +0530

    commit

---
+++ arithmeticOperators.py
@@ -0,0 +1,6 @@
+a = int(input("a = "))
+b = int(input("b = "))
+print("The value of a+b is: ",a+b)
+print("The value of a-b is: ",a-b)
+print("The value of a*b is: ",a*b)
+print("The value of a//b is: ",a//b)
