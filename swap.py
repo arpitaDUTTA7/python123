f0c15d680f77f3568b43e01bd4d4f0d80a811035
ArpitaD <arpitadutta670@gmail.com>
2022-10-30 00:33:12 +0530

    commit

--- swap.py
+++ swap.py
@@ -1,14 +1,11 @@
-from re import X
-
-
-a = int(input("Enter 1st num "))
-b = int(input("Enter 2nd num "))
-print("Before swapping")
+a = int(input("Enter 1st num: "))
+b = int(input("Enter 2nd num: "))
+print("Before swapping: ")
 print ("a = ",a)
 print ("b = ",b)
 temp = a
 a = b
 b = temp
-print ("After swapping")
+print ("After swapping: ")
 print ("a = ",a)
 print ("b = ",b)
