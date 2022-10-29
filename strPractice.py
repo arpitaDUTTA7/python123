2d62463f0f82fdd931d1ce8671b105eeab821d48
ArpitaD <arpitadutta670@gmail.com>
2022-10-29 23:47:48 +0530

    commit

--- strPractice.py
+++ strPractice.py
@@ -3,8 +3,4 @@ print("First letter",name[0])
 print("Second letter",name[-1])
 i=int(name.__len__())
 j=int(name.__len__()/2)
-k=int(((name.__len__())+1)/2)
-if i%2==0 :
-   print("Middle letter",name[j])
-else :
-   print("Middle letter",name[k])
+print("Middle letter",name[j])
