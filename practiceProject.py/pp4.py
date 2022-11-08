f2164c25ca883d10f527b0b20a64b83414a5467a
ArpitaD <arpitadutta670@gmail.com>
2022-11-08 08:12:00 +0530

    commit

--- practiceProject.py/pp4.py
+++ practiceProject.py/pp4.py
@@ -2,14 +2,18 @@
 n = int(input("Enter the last term: "))
 t1 = 0
 t2 = 1 
-print("0",end="\n")
-print("1",end="\n")
-for i in range(2,n):
-    t=t1+t2
-    print(t)
-    t1=t2
-    t2=t
-    i+=1
+if n == 1: 
+   print(t1,end="\n")
+
+else:
+    print(t1,end="\n")
+    print(t2,end="\n")
+    for i in range(2,n):
+       t=t1+t2
+       print(t)
+       t1=t2
+       t2=t
+       i+=1
 
 
 
